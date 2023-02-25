3dc5d3c7ff0655dff6529fc1f05e1d79b2c33380
Jacob Cell <59305245+Thirstymidget28@users.noreply.github.com>
2023-02-25 11:34:39 -0600

    changes

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const Flex = () => {
   return (
-    <View style={styles.container}>
-      <Text>Food Finders</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container, {flexDirection: 'column'},]}>
+        <View style= {{flex: 1, backgroundColor: 'red'}} />
+        <View style= {{flex: 2, backgroundColor: 'red'}} />
+        <View style= {{flex: 3, backgroundColor: 'red'}} />
+
     </View>
   );
 }
 
+/*
+export default function App() {
+  return (
+    <View style={[styles.container, {flexDirection: 'column'},]}>
+        <View style= {{flex: 1, backgroundColor: 'red'}} />
+        <View style= {{flex: 2, backgroundColor: 'red'}} />
+        <View style= {{flex: 3, backgroundColor: 'red'}} />
+
+    </View>
+  );
+}
+*/
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 20,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+export default Flex;
