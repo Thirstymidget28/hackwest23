f16a67e096876c3dbf620324fb2646f669bd5e12
Jacob Cell <59305245+Thirstymidget28@users.noreply.github.com>
2023-02-25 14:15:13 -0600

    made filter button
    Not functional

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {View, TouchableOpacity, Text, StyleSheet} from 'react-native';
+import {View, Image, TouchableOpacity, Text, StyleSheet} from 'react-native';
 
 const mainMenuLayout = () => {
   return ( // Create a Flex space that can be filled with UI elements.
@@ -14,10 +14,19 @@ const mainMenuLayout = () => {
 
 const PreviewLayout = ({
   label,
+  children,
+  values,
+  selectedValue,
+  setSelectedValue,
 }) => (
   <View style={{flex: 1}}>
     <View style= {{flex: 1/8, backgroundColor: 'red'}}>
+      <TouchableOpacity style={[styles.filter, {zIndex: 1}]}>
+        <Image style={[styles.gearImg, {zIndex: 2}]}
+        source = {require('./assets/gear-icon.png')}/>
+      </TouchableOpacity>
       <Text style={styles.label}>{label}</Text>
+
     </View>
     <View style= {{flex: 1/200, backgroundColor: 'black'}} />
     
@@ -42,11 +51,22 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   label: {
+    marginTop: -35,
     textAlign: 'center',
-    marginTop: 45,
     fontSize: 24,
     fontWeight: 'bold',
   },
+  filter: {
+    alignSelf: 'flex-end',
+    marginRight: 10,
+    marginTop:50,
+    width: 35,
+    height: 35,
+  },
+  gearImg: {
+    width: 30,
+    height: 30,
+  }
 });
 
 export default mainMenuLayout;
