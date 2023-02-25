2b76e602c9382e1ad4064ab10dae760e8c7d0526
Adriana <100482041+AdriaSilva@users.noreply.github.com>
2023-02-25 11:28:16 -0600

    Update App.js

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Food poop</Text>
+      <Text>Food Finder</Text>
       <StatusBar style="auto" />
     </View>
   );
